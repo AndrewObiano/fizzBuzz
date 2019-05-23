10520b8c59a2f8d73f262f86f92f6e7132681be9
Andrew Obiano <andrew.obiano@gmail.com>
2019-05-22 18:39:59 -1000

    finished

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ if the number is a multiple of 3, it will console.log "fizz",
 if the number is a multiple of 5, it will console.log "buzz",  
 if the number is a multiple of 3 and 5, it will console.log "fizzBuzz"
 */
+
+for(i=1; i<=100; i++){
+    if(i%15 === 0){
+        console.log('fizzBuzz');
+    }else if(i%5 === 0){
+        console.log('buzz');
+    }else if(i%3 === 0){
+            console.log('fizz');
+    }else{
+        console.log(i);
+    }
+}
